6e66637dc3cdd54d8a7dde4d02f3fefb49af52f7
Michael Ridgway <mridgway@yahoo-inc.com>
2015-05-21 13:32:03 -0700

    Export built react-server instead of source

--- register.js
+++ register.js
@@ -5,7 +5,7 @@
 'use strict';
 
 // Ensure the cache for react-server is populated
-require('./index');
+require('./dist/react-server');
 
-require.cache[require.resolve('react')] = require.cache[require.resolve('./index')];
-require.cache[require.resolve('react/addons')] = require.cache[require.resolve('./index')];
+require.cache[require.resolve('react')] = require.cache[require.resolve('./dist/react-server')];
+require.cache[require.resolve('react/addons')] = require.cache[require.resolve('./dist/react-server')];
